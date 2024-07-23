1ac244159b8e44bbf17c8836542420e2aca72f29
Administrator <Administrator@172.31.36.165>
2024-07-23 14:37:57 +0000

    advance streams & scenarios

--- introduction/src/introduction/Sortingwebelementstreams.java
+++ introduction/src/introduction/Sortingwebelementstreams.java
@@ -13,49 +13,50 @@ public class Sortingwebelementstreams {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-WebDriver driver = new ChromeDriver();
-		
+
+		WebDriver driver = new ChromeDriver();
+
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
-		
+
 		driver.findElement(By.xpath("//tr/th[1]")).click();
-		
-		
-		
-		/*List<String>actualList=elements.stream().map(s->s.getText()).collect(Collectors.toList());
-		
-		List<String>listAfterSort=actualList.stream().sorted().collect(Collectors.toList());
-		
-		Assert.assertTrue(actualList.equals(listAfterSort));*/
-		
-		List<String>priceOfVegiige;
-		
+
+		/*
+		 * List<String>actualList=elements.stream().map(s->s.getText()).collect(
+		 * Collectors.toList());
+		 * 
+		 * List<String>listAfterSort=actualList.stream().sorted().collect(Collectors.
+		 * toList());
+		 * 
+		 * Assert.assertTrue(actualList.equals(listAfterSort));
+		 */
+
+		List<String> priceOfVegiige;
+
 		do {
-			List<WebElement>elements=driver.findElements(By.xpath("//tbody/tr/td[1]"));
-			
-			priceOfVegiige=elements.stream().filter(s->s.getText().contains("Potato")).map(s->getVaggies(s)).collect(Collectors.toList());
-			
-			priceOfVegiige.forEach(a->System.out.println(a));
-			
-			if(priceOfVegiige.size()<1) {
-				
+			List<WebElement> elements = driver.findElements(By.xpath("//tbody/tr/td[1]"));
+
+			priceOfVegiige = elements.stream().filter(s -> s.getText().contains("Potato")).map(s -> getVaggies(s))
+					.collect(Collectors.toList());
+
+			priceOfVegiige.forEach(a -> System.out.println(a));
+
+			if (priceOfVegiige.size() < 1) {
+
 				driver.findElement(By.xpath("//a[@aria-label='Next']")).click();
-				
+
 			}
-			
-		}while(priceOfVegiige.size()<1);
-		
-		
+
+		} while (priceOfVegiige.size() < 1);
 
 	}
+
 	public static String getVaggies(WebElement s) {
-		
-		String beanValue=s.findElement(By.xpath("following-sibling::td[1]")).getText();
-		
-		return beanValue ; 
-		
-		
+
+		String beanValue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+
+		return beanValue;
+
 	}
 
 }
