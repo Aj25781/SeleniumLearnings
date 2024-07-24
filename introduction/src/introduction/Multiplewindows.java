8541a9e84309676ec0bb479f49c5ae1a8b98325e
Administrator <Administrator@172.31.36.165>
2024-07-24 19:32:34 +0000

    this is multiple windows

--- introduction/src/introduction/Multiplewindows.java
+++ introduction/src/introduction/Multiplewindows.java
@@ -1,21 +1,26 @@
 package introduction;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Multiplewindows {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://www.rahulshettyacademy.com/angularpractice/");
-		driver.findElement(By.xpath("//input[@name='name']"));
+		WebElement nameElement=driver.findElement(By.xpath("//input[@name='name']"));
 		
 		driver.switchTo().newWindow(WindowType.TAB);
 		Set<String>si=driver.getWindowHandles();
@@ -29,7 +34,11 @@ public class Multiplewindows {
 .getText();	
 		
 		driver.switchTo().window(parentWindowId);
-		driver.findElement(By.xpath("//input[@name='name']")).sendKeys(newText);
+		nameElement.sendKeys(newText);
+		
+		File newFileObject=nameElement.getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(newFileObject, new File("ajay.png"));
 	}
 
 }
